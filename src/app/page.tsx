fc42fb65d51db251552cd4db85ac140c0f858c0b
MicroProyects <microproyects@gmail.com>
2025-01-18 23:31:41 -0500

    probando xd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <h1>PMP <span>Papu Micro Proyects</span></h1>
       </main>
       <footer className={styles.footer}>
-
+        <p> Todos los derechos reservados por el CEO xd</p>
       </footer>
     </div>
   );
